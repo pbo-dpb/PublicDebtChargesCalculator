a777dae0659d8524b96494d0acc24b34022a1bca
Rémy Vanherweghem <remy.vanherweghem@parl.gc.ca>
2023-11-27 19:22:43 +0000

    Removed debugbar references.

--- src/components/DebugBar.vue
+++ src/components/DebugBar.vue
@@ -1,25 +1,14 @@
 <template>
     <section class="border-y -mx-8 border-gray-300 px-8 py-4 my-8 bg-gray-100 dark:bg-gray-950 flex flex-col gap-4">
         <div class="flex flex-row justify-between items-center">
-            <div class="font-mono text-gray-800 dark:text-white text-center">🔧 DEBUG</div>
+            <div class="font-mono text-gray-800 dark:text-white text-center">🔧 Current client payload: {{
+                currentlyLoadedPayloadLastUpdatedDate }}</div>
             <Button @click="switchLanguage">{{
                 language ==
                 'en'
                 ? 'fr' : 'en' }}</Button>
         </div>
-        <div class="flex flex-col gap-2 text-sm font-mono">
-            <div>{{ currentlyLoadedPayloadLastUpdatedDate }} <span v-if="!hasCustomPayload">(current | <a
-                        class="underline text-blue-800" :href="payloadUrl" :download="payloadUrl">download</a>)</span><span
-                    v-else>(custom file)</span>
-            </div>
-            <hr>
-            <label>Replace by custom file (.xlsx)<br>
-                <input type="file" @change="handleFileUpload" accept=".xlsx" />
-            </label>
 
-
-
-        </div>
     </section>
 </template>
   
@@ -35,7 +24,6 @@ export default {
     data() {
         return {
             payloadUrl,
-            hasCustomPayload: false
         }
     },
     components: {
@@ -46,13 +34,7 @@ export default {
         return { workbookStore }
     },
     methods: {
-        handleFileUpload(event) {
-            const file = event.target.files[0]
-            if (file) {
-                this.workbookStore.loadWorkbook(file)
-                this.hasCustomPayload = true
-            }
-        },
+
         switchLanguage() {
             this.language = (this.language == 'en') ? 'fr' : 'en'
         }
